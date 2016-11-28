559b9d839ab68ebad2c4b1095799376495048f09
sunghyunpark <sunghyun1038@naver.com>
2016-11-28 13:47:46 +0900

    액티비티 및 애니메이션 추가

---
+++ app/src/main/java/page3/Upload_Page1.java
@@ -0,0 +1,27 @@
+package page3;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.seedteam.latte.R;
+
+public class Upload_Page1 extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upload_page1);
+
+
+    }
+
+}
