36e1eeb8c94ceb390805567302c390bae55a45da
sunghyunpark <sunghyun1038@naver.com>
2016-12-13 23:16:41 +0900

    refresh_result 추가

--- app/src/main/java/rest/TimelineResponse.java
+++ app/src/main/java/rest/TimelineResponse.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class TimelineResponse {
 
     private ArrayList<Article> article;
+    private boolean refresh_result;    //새로운 아티클이 있는지 여부 
     private boolean error;
     public String error_msg;
 
@@ -18,6 +19,14 @@ public class TimelineResponse {
 
     public void setArticle(ArrayList<Article> article) {
         this.article = article;
+    }
+
+    public boolean isRefresh_result() {
+        return refresh_result;
+    }
+
+    public void setRefresh_result(boolean refresh_result) {
+        this.refresh_result = refresh_result;
     }
 
     public boolean isError() {
