0ffce306d73d8d1cdd43ac86fb70800826a48270
sunghyunpark <sunghyun1038@naver.com>
2016-12-26 13:43:43 +0900

    댓글 화면에서 작성자의 uid로댓글 작성되는 이슈 수정

--- app/src/main/java/page2/Fragment_Follow_Timeline.java
+++ app/src/main/java/page2/Fragment_Follow_Timeline.java
@@ -474,7 +474,7 @@ public class Fragment_Follow_Timeline extends Fragment implements SwipeRefreshLa
                         detail_pos = position;
                         detail_article_id = currentItem.getArticle_id();
                         Intent intent = new Intent(getActivity(), Article_Comment_Activity.class);
-                        intent.putExtra("user_uid", currentItem.getUid());
+                        intent.putExtra("user_uid", uid);
                         intent.putExtra("article_id", currentItem.getArticle_id());
                         startActivity(intent);
                         getActivity().overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
