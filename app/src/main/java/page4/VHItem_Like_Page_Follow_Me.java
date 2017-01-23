1a794ce549dc55344c832616f7dfb5646ae9995c
sunghyunpark <sunghyun1038@naver.com>
2017-01-23 18:26:54 +0900

    좋아요>내게시글에서 나를 팔로우 하는 경우 VHitem 누락되어 추가

--- app/src/main/java/page4/VHItem_Like_Page_Follow_Me.java
+++ app/src/main/java/page4/VHItem_Like_Page_Follow_Me.java
@@ -7,20 +7,21 @@ import android.widget.TextView;
 
 import com.seedteam.latte.R;
 
+/*
+    좋아요 화면에서 내 게시물 탭 -> 상대방이 나를 팔로우한 경우
+ */
 class VHItem_Like_Page_Follow_Me extends RecyclerView.ViewHolder{
 
     ImageView profile_img;
     TextView content_txt;
-    TextView comment_txt;
-    ImageView content_pic;
+    ImageView follow_btn;
 
 
     public VHItem_Like_Page_Follow_Me(View itemView) {
         super(itemView);
         this.profile_img = (ImageView)itemView.findViewById(R.id.profile_img);
         this.content_txt = (TextView)itemView.findViewById(R.id.content_txt);
-        this.comment_txt = (TextView)itemView.findViewById(R.id.comment_txt);
-        this.content_pic = (ImageView)itemView.findViewById(R.id.content_pic);
+        this.follow_btn = (ImageView)itemView.findViewById(R.id.follow_btn);
 
     }
 }
